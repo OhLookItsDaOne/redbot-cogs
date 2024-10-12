289b42326026540066a94bc79675e7ab9c5bd31e
OhLookItsDaOne <53413176+OhLookItsDaOne@users.noreply.github.com>
2024-10-12 22:21:49 +0200

    Update mycog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -22,7 +22,7 @@ class ForumPostNotifier(commands.Cog):
     def create_troubleshooting_message(self):
         """Creates the troubleshooting message."""
         message = (
-            f"Hello! 👋\n\n"
+            Hello! 👋
             Provide info to help us help you
             ```
             Please answer all these, unless we say otherwise-
